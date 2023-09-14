66aa787a3b47f66c52a271f70bd41f0a07961c96
W.Xaver <w.xaver@googlemail.com>
2023-09-14 11:08:13 +0200

    module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module mtviewer {
+    opens de.p2tools.mtviewer;
+    exports de.p2tools.mtviewer;
+    
+    opens de.p2tools.mtviewer.controller.data;
+
+
+    requires de.p2tools.p2lib;
+    requires javafx.controls;
+    requires org.controlsfx.controls;
+
+    requires java.logging;
+    requires java.desktop;
+
+    requires commons.cli;
+    requires com.fasterxml.jackson.core;
+    requires org.tukaani.xz;
+
+    requires okhttp3;
+}
+
